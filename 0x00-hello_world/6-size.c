7120a558fc79448fd572978c3191310043f952da
FatmaAtta <fatmamaali@gmail.com>
2023-03-17 08:25:38 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,8 +12,8 @@ long long longlongint;
 float floating;
 printf("Size of a char: %lu byte(s)\n", sizeof(character));
 printf("Size of an int: %lu byte(s)\n", sizeof(integer));
-printf("Size of long int: %lu byte(s)\n", sizeof(longint));
-printf("Size of long long int: %lu byte(s)\n", sizeof(longlongint));
+printf("Size of a long int: %lu byte(s)\n", sizeof(longint));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(longlongint));
 printf("Size of a float: %lu byte(s)\n", sizeof(floating));
 return (0);
 }
