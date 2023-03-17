13f355692a592f3cc44a858d497a8bccc0c7b701
FatmaAtta <fatmamaali@gmail.com>
2023-03-17 04:38:47 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-int main(){
-puts("Programming is like building a multilingual puzzle\n")
-	return 0;
+/**
+ * main - printed a statement using puts() function.
+ * I followed the betty style rules
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
 }
