252108ae8b39950adee0a65069062f9c0a399ae6
FatmaAtta <fatmamaali@gmail.com>
2023-03-17 05:05:26 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - printed a statement using printf()
+ * Return: 0
+ * used betty style
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
