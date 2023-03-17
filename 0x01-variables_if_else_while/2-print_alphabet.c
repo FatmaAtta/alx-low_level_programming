d598b04ea9dc12238c5d0f57a5331cd195d2e44b
FatmaAtta <fatmamaali@gmail.com>
2023-03-17 15:45:56 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - used a for loop to print all letters
+ * Return: 0
+ */
+int main(void)
+{
+for (int i = 97; i <= 122; i++)
+{
+putchar(i);
+putchar('\n');
+}
+return (0);
+}
+
