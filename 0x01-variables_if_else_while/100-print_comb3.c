8d84c25a9589ecac97d081e5a36ce2a8a3fd1aad
FatmaAtta <fatmamaali@gmail.com>
2023-03-20 14:37:06 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - used for loops
+ * Return: 0
+ */
+int main(void)
+{
+int i, j;
+for (i = 48; i <= 57; i++)
+{
+for (j = i + 1; j <= 57; j++)
+{
+putchar(i);
+putchar(j);
+putchar(44);
+putchar(32);
+}
+}
+putchar('\n');
+return (0);
+}
