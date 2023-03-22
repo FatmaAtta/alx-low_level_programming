240da324be099c4a5e42e22c63ca7f9e680022e7
FatmaAtta <fatmamaali@gmail.com>
2023-03-21 18:18:56 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - prints all alphabets 10 times
+ */
+void print_alphabet_x10(void)
+{
+char l;
+int i;
+for (i = 1; i <= 10; i++)
+{
+for (l = 'a'; l <= 'z'; l++)
+{
+_putchar(l);
+}
+_putchar('\n');
+}
+}
