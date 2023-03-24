4f6728804b8f1b3323adda964017a4c6ef310166
FatmaAtta <fatmamaali@gmail.com>
2023-03-24 15:42:15 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,13 +7,13 @@ void print_most_numbers(void)
 int i;
 for (i = 48; i <= 57; i++)
 {
-if (i != 50 || i != 52)
+if (i == 50 || i == 52)
 {
-_putchar(i);
+continue;
 }
 else
 {
-continue;
+_putchar(i);
 }
 }
 _putchar('\n');
