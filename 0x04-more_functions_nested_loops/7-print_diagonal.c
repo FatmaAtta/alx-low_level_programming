f2309822105c86ce185b34c7cd1fb5d60d47ee87
FatmaAtta <fatmamaali@gmail.com>
2023-03-24 16:38:16 -0700

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_diagonal - prints a diagonal on the screen
+ * @n: length of diagonal
+ */
+void print_diagonal(int n)
+{
+int i, j;
+if (n > 0)
+{
+for (i = 1; i <= n; i++)
+{
+for (j = 1; j <= i; j++)
+{
+_putchar(32);
+}
+_putchar(92);
+_putchar('\n');
+}
+}
+_putchar('\n');
+}
