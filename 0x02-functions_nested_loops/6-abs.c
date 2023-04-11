9a5a185463de8af91bc293e181e0bbe85ec498d4
FatmaAtta <fatmamaali@gmail.com>
2023-04-11 14:35:25 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _abs - check the code
+* @n: integer to be positive
+* Return: Absolute value.
+*/
+int _abs(int n)
+{
+if (n < 0)
+{
+return (-n);
+}
+else
+{
+return (n);
+}
+}
